67df68f8ac3e48bfe7bfbf9cb0d7bdfbd0d3dd96
Jack Hansjee <jack@hansjee.com>
2024-11-06 22:32:21 +0000

    Refactor comment cards to reduce number of api calls for users' avatars

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,20 +1,12 @@
-import { useEffect, useState } from "react";
-import { apiClient } from "../utils/apiClient";
 import { commentCard, commentHeader } from "../styles/CommentCard.module.css";
 
-export const CommentCard = ({ comment }) => {
-  const { body, author, votes } = comment;
-  const [commenter, setCommenter] = useState({});
-  useEffect(() => {
-    apiClient
-      .get(`/users/${author}`)
-      .then(({ data }) => setCommenter(data.user));
-  }, []);
+export const CommentCard = ({ comment, author }) => {
+  const { body, votes } = comment;
   return (
     <li className={commentCard}>
       <div className={commentHeader}>
-        <img src={commenter.avatar_url} className="avatar avatar-small" />
-        <span>{author}</span>
+        <img src={author?.avatar_url} className="avatar avatar-small" />
+        <span>{author?.username}</span>
         <span>Votes: {votes}</span>
       </div>
       {body}
