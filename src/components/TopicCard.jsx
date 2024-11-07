bd8f69abe019b00e284290c605af327c7c5d7a37
Jack Hansjee <jack.hansjee@me.com>
2024-11-07 11:47:17 +0000

    Add styling so topic slug appears on top of image

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import { articleCard, articleImg } from "../styles/ArticleCard.module.css";
+import {
+  topicSlug,
+  imgWithSlug,
+  description,
+} from "../styles/TopicCard.module.css";
 
 export const TopicCard = ({ topic }) => {
   return (
     <Link to={`/?topic=${topic.slug}`} className="link">
       <div className={articleCard}>
-        <img src={`/src/imgs/${topic.slug}.jpg`} className={articleImg} />
-        <h3>{topic.slug}</h3>
-        <p>{topic.description}</p>
+        <div className={imgWithSlug}>
+          <img src={`/src/imgs/${topic.slug}.jpg`} className={articleImg} />
+          <span className={topicSlug}>{topic.slug}</span>
+        </div>
+        <p className={description}>{topic.description}</p>
       </div>
     </Link>
   );
